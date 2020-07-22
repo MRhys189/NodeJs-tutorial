0c529abe551f035ca89335901a9ce68d4385a9d5
McRhys189 <muragerhys189@gmail.com>
2020-07-22 10:12:54 +0300

    Events Arguments

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
-emitter.on('messageLogged', function(){
-    console.log('Listener called');
+emitter.on('messageLogged', function(arg){
+    console.log('Listener called', arg);
 })
 
-emitter.emit("messageLogged");
+emitter.emit("messageLogged", { id:1, url: 'http://'}); //the {} is event argument
+
+// Raise: logging (data: message)
+
+emitter.on('Exercise logged', function(arg){
+    console.log('Exercise received', arg);
+})
+
+emitter.emit('Exercise logged', {id: 2, url: 'Has done the question stated'});
