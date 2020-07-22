1810c199efa119baf5a9331b248d6c7c76a3fc38
McRhys189 <muragerhys189@gmail.com>
2020-07-22 10:38:14 +0300

    Extending Events Emitter

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
 
-emitter.on('messageLogged', function(arg){
+const Logger = require('./logger');
+const logger = new Logger;
+
+logger.on('messageLogged', function(arg){
+    //after logger registering message, execute code below
     console.log('Listener called', arg);
-})
+});
 
-emitter.emit("messageLogged", { id:1, url: 'http://'}); //the {} is event argument
+logger.log('message');
 
-// Raise: logging (data: message)
-
-emitter.on('Exercise logged', function(arg){
-    console.log('Exercise received', arg);
-})
-
-emitter.emit('Exercise logged', {id: 2, url: 'Has done the question stated'});
--- logger.js
+++ logger.js
@@ -1,11 +1,23 @@
-console.log(__dirname);
-console.log(__filename);
+
+const EventEmitter = require('events');
+// delete emtter object
+
 var url  = 'http://mylogger.io/log';
 
-function log(message) {
-    //send an HTTP request
-    console.log(message);
+class Logger extends EventEmitter {
+    log(message) {
+        //send an HTTP request
+        console.log(message);
+    
+        //Raise event after logging message
+        //logger logs message so should be here instead of app module
+        this.emit("messageLogged", { id:1, url: 'http://'}); //the {} is event argument
+        //no longer need emmiter.emit() due to inheritance
+    }
+
+
 }
 
-module.exports = log;
+
+module.exports = Logger;
 
